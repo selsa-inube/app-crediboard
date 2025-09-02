aab0cefcf0a7a9ba03eb77b03b93e22a8647f372
Miller Ostos <millerandresoc@gmail.com>
2025-09-02 10:53:29 -0500

    refactor: adjuest the logict to send de values from select

--- src/pages/board/outlets/financialReporting/ToDo/DecisionModal/index.tsx
+++ src/pages/board/outlets/financialReporting/ToDo/DecisionModal/index.tsx
@@ -13,14 +13,15 @@ import {
 import { BaseModal } from "@components/modals/baseModal";
 import { makeDecisions } from "@services/creditRequest/command/makeDecisions";
 import { validationMessages } from "@validations/validationMessages";
+import { decisions } from "@services/enum/icorebanking-vi-crediboard/decisions/decisions";
 
-import { IMakeDecisionsCreditRequestWithXAction } from "./types";
+import { IMakeDecisionsCreditRequestWithXAction, IMakeDecisionsPayload } from "./types";
 import { StyledContainerTextField } from "./styles";
 import { soporteInvalidOptions, txtFlags, txtOthersOptions } from "./../config";
 
 interface FormValues {
   textarea: string;
-  selectedOptions?: string[];
+  selectedOptions?: string;
 }
 
 export interface DecisionModalProps {
@@ -63,22 +64,49 @@ export function DecisionModal(props: DecisionModalProps) {
     textarea: readOnly
       ? Yup.string()
       : Yup.string()
-          .max(maxLength, validationMessages.maxCharacters(maxLength))
-          .required(validationMessages.required),
+        .max(maxLength, validationMessages.maxCharacters(maxLength))
+        .required(validationMessages.required),
   });
 
-  const sendData = async (value: string) => {
+  const handleNonCompliantDocuments = (formValues: FormValues): string[] => {
+    let selectedOptions: string[] | number[] = [];
+
+    if (formValues.selectedOptions) {
+      selectedOptions = formValues.selectedOptions.split(",");
+      selectedOptions?.shift();
+    }
+
+    selectedOptions = selectedOptions.map(option => parseInt(`${option}`) - 1);
+
+    return realNamesEnumNonCompliantDocuments(selectedOptions) as string[];
+  }
+
+  const realNamesEnumNonCompliantDocuments = (selectedOptions: number[]) => {
+    let valuesFromEnum = selectedOptions.map(
+      (option) => decisions[9].nonCompliantDocuments?.[option]
+    );
+
+    return valuesFromEnum;
+  }
+  const sendData = async (formValues: FormValues) => {
     try {
+      const makeDecisionsPayload: IMakeDecisionsPayload = {
+        creditRequestId: data.makeDecision.creditRequestId,
+        humanDecision: data.makeDecision.humanDecision,
+        justification: formValues.textarea,
+      };
+
+      if (formValues.selectedOptions && data.xAction === "DisapproveLegalDocumentsAndWarranties") {
+        makeDecisionsPayload["nonCompliantDocuments"] = handleNonCompliantDocuments(formValues);
+      }
+
       const response = await makeDecisions(
         data.businessUnit,
         data.user,
-        {
-          creditRequestId: data.makeDecision.creditRequestId,
-          humanDecision: data.makeDecision.humanDecision,
-          justification: value,
-        },
+        makeDecisionsPayload,
         data.xAction
       );
+
       if (response?.statusServices === 200) {
         navigate("/");
         addFlag({
@@ -109,7 +137,7 @@ export function DecisionModal(props: DecisionModalProps) {
 
   const initialValues: FormValues = {
     textarea: "",
-    selectedOptions: [],
+    selectedOptions: "",
   };
 
   return (
@@ -122,7 +150,7 @@ export function DecisionModal(props: DecisionModalProps) {
       ) => {
         onSubmit?.(values);
         setSubmitting(false);
-        sendData(values.textarea);
+        sendData(values);
       }}
     >
       {({ errors, touched, handleSubmit, values }) => (
