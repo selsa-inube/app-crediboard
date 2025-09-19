c45693bbe218b2395ba22d93ff87a9066d43a1ad
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2025-09-19 13:35:04 -0500

    feat: integrate service riskscoring

---
+++ src/pages/board/outlets/creditProfileInfo/RiskScoring/config.ts
@@ -0,0 +1,7 @@
+export const dataRiskScoring = {
+  totalScore: "Puntaje total",
+  noData: "Datos no encontrados",
+  noDataDescription: "No pudimos obtener los datos solicitados.",
+  retryButton: "Reintentar",
+  modalError: "No se pudo obtener el scoring. Por favor, intente nuevamente.",
+};
