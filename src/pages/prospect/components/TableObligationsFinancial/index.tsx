0f09953ac0a581af9464833209b47348fdf469e4
Miller Ostos <millerandresoc@gmail.com>
2025-09-26 10:19:15 -0500

    refactor: delete useEffect

--- src/pages/prospect/components/TableObligationsFinancial/index.tsx
+++ src/pages/prospect/components/TableObligationsFinancial/index.tsx
@@ -260,12 +260,6 @@ export const TableFinancialObligations = (
     }
   }, [selectedBorrower, dataProspect, filterListBorrowersFinancialObligation]);
 
- /*  useEffect(() => {
-    if (newObligation) {
-      saveNewObligation();
-    }
-  }, [newObligation, saveNewObligation]); */
-
   useEffect(() => {
     setCurrentPage(0);
   }, [selectedBorrower]);
