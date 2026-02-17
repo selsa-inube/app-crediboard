2a9d84e639da6eb99bf4335453acfb939b96a49f
miklos12 <millerandresoc@gmail.com>
2026-02-17 14:46:54 -0500

    fix: use getCreditLineGeneralTerms service to validations on update product

--- src/services/lineOfCredit/types.ts
+++ src/services/lineOfCredit/types.ts
@@ -1,3 +1,15 @@
 export interface ISearchAllModesOfDisbursementTypes {
   modesOfDisbursementTypes: string[];
 }
+
+export interface CreditLineGeneralTerms {
+  abbreviateName: string;
+  description: string;
+  minTerm: number;
+  maxTerm: number;
+  minAmount: number;
+  maxAmount: number;
+  minEffectiveInterestRate: number;
+  maxEffectiveInterestRate: number;
+  amortizationType: string[];
+}
